3d56559c0dd3ed4c287a166a244d80ba56712177
obrennomartins <7295761+obrennomartins@users.noreply.github.com>
2021-04-04 23:30:01 -0300

    Adicionado serviço commentWatcher.js

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 ![version](https://img.shields.io/badge/version-0.1.0-blue)
 [![Codacy Badge](https://app.codacy.com/project/badge/Grade/49ba3ec324d04f789f4650d07dd5434e)](https://www.codacy.com/gh/instabotix/services/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=instabotix/services&amp;utm_campaign=Badge_Grade)
+
+## Serviços
+
+- [`productFinder.js`](productFinder.js) fornece o ID dos produtos mostrados em cada mídia do Instagram de acordo com o 
+  que foi cadastrado na plataforma da VTEX
+- [`commentWatcher.js`](commentWatcher.js) verifica as notificações da conta do Instagra, selenciona apenas os 
+  comentários e encaminha para o chatbot
---
+++ commentWatcher.js
@@ -0,0 +1,64 @@
+const Instagram = require('instagram-web-api');
+const mongoose = require('mongoose');
+
+
+// Obtendo as variáveis de ambiente
+const MONGOBD_URL = process.env.MONGOBD_URL;
+const [ username, password ] = [process.env.INSTAGRAM_USERNAME, process.env.INSTAGRAM_PASSWORD];
+
+
+// Configurando o banco de dados
+mongoose.connect(MONGOBD_URL, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro no banco de dados ' + err);
+});
+
+// Esquema da coleção que registra os comentários que já foram encaminhados para o chatbot
+const comentarioSchema = new mongoose.Schema({
+    comentario: {type: String, unique: true}
+}, {collection: 'comentarios'});
+
+// Registra o modelo no banco de dados
+const Comentarios = mongoose.model('Comentarios', comentarioSchema);
+
+
+// Biblioteca da API não-oficial do Instagram
+const client = new Instagram({username, password});
+
+(async () => {
+    // await client.login();
+    const atividade = await client.getActivity();
+    const notificacoes = atividade.activity_feed.edge_web_activity_feed.edges;
+
+    notificacoes.forEach((notificacao) => {
+        // Seleciona apenas as notificação referentes a comentários
+        if (notificacao.node.__typename === 'GraphCommentMediaStory') {
+            const comentario_id = notificacao.node.id.split('_').slice(1).join('_');
+            // Procura no banco de dados se o comentário já foi endereçado
+            Comentarios.find({comentario: comentario_id}, (err, doc) => {
+                if (doc.length === 0) {
+                    // Seleciona os detelhes do comentário para enviar para o chatbot
+                    Comentarios.create({comentario: comentario_id}, (err, doc) => {
+                        let comentario = {
+                            comentario_id: comentario_id,
+                            timestamp: notificacao.node.timestamp,
+                            usuario: {
+                                id: notificacao.node.user.id,
+                                usuario: notificacao.node.user.username,
+                                nome_completo: notificacao.node.user.full_name
+                            },
+                            media:  notificacao.node.media.shortcode,
+                            texto: notificacao.node.text
+                        };
+                        return comentario;
+                    });
+                }
+            });
+        }
+    });
+})();
